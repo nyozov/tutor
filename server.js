3bb4283be19156fd39eea7f8488a120f7f8a33cf
Nikolay Yozov <nikolay@Nikolays-MacBook-Air.local>
2026-03-18 14:44:52 -0400

    express boilerplate

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+
+
+const app = express();
+
+app.use(cors())
+
+
+const port = 3000;
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
